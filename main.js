ef3b4852bfec82284aa6683a9ce32935defad13d
Mohan dash <158100710+Maya12981@users.noreply.github.com>
2025-04-03 17:27:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ addEventListener('fetch', event => {
 });
 
 // Add your bot token here.
-const BOT_TOKEN = "bot token here";
+const BOT_TOKEN = "7416202490:AAEeO-Q9WuMZqjMO1UeNyv8LaiBXsckVZLM";
 
 //Pro people: don't change this else your bot will be fucked.
 const API_BASE_URL = "https://probable-berti-frozenbotspvt-17e82b7b.koyeb.app";
